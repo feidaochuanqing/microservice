189036e83f7fbf234e785a9eefa1cf813fcb206b
feidaochuanqing <feidaochuanqing@126.com>
2017-12-25 17:36:13 +0800

    add adminlte layout

--- microservice-front/src/js/index.js
+++ microservice-front/src/js/index.js
@@ -47,52 +47,94 @@ export default class IndexComponent extends React.Component {
     if (this.state.path.length > 0) {
       path = this.state.path;
     }
-    return <div>
-
-      <Layout>
-        <Header className="header" style={{ background: 'white' }}>
-          <div >
-            <div className="logo">微服务架构</div>
+    return <div className="wrapper" id="example">
+    <header className="main-header">
+      <a href="#" className="logo">
+        <span className="logo-mini"><b>A</b>LT</span>
+        <span className="logo-lg"><b>微服务框架</b></span>
+      </a>
+      <nav className="navbar navbar-static-top">
+        <a href="#" className="sidebar-toggle" data-toggle="push-menu" role="button">
+          <span className="sr-only">Toggle navigation</span>
+        </a>
+  
+        <div className="navbar-custom-menu">
+          <ul className="nav navbar-nav">
+            <li className="dropdown user user-menu">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+                <img src="src/images/user2-160x160.jpg" className="user-image" alt="User Image" />
+                <span className="hidden-xs">王小明</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  
+    <aside className="main-sidebar">
+      <section className="sidebar">
+        <div className="user-panel">
+          <div className="pull-left image">
+            <img src="src/images/user2-160x160.jpg" className="img-circle" alt="User Image" />
           </div>
-        </Header>
-        <Layout>
-          <Sider width={200} style={{ background: '#fff' }}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1', 'sub2']}
-              style={{ height: '100%' }}
-            >
-              <SubMenu key="sub1" title={<span><Icon type="user" />产品管理</span>}>
-                <Menu.Item key="1">
-                  <a component="ProductListComponent" visitorPath="系统首页/主数据/产品列表" onClick={this.nav.bind(this)}>产品列表</a>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <a component="ProductCategoryListComponent" visitorPath="系统首页/主数据/产品类别列表" onClick={this.nav.bind(this)}>产品类别列表</a>
-                </Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="laptop" />交易管理</span>}>
-                <Menu.Item key="5">
-                  <a component="OrderListComponent" visitorPath="系统首页/交易管理/订单列表" onClick={this.nav.bind(this)}>订单列表</a>
-                </Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Sider>
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '12px 0' }}>
+          <div className="pull-left info">
+            <p>王小明</p>
+            <a href="#"><i className="fa fa-circle text-success"></i> 在线</a>
+          </div>
+        </div>
+        
+        <ul className="sidebar-menu" data-widget="tree">
+          <li className="header">导航</li>
+           
+          <li className="treeview">
+            <a href="#">
+              <i className="fa fa-table"></i> <span>产品管理</span>
+              <span className="pull-right-container">
+                <i className="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul className="treeview-menu">
+              <li><a component="ProductListComponent" visitorPath="系统首页/主数据/产品列表" onClick={this.nav.bind(this)}><i className="fa fa-circle-o"></i> 产品列表</a></li>
+              <li><a component="ProductCategoryListComponent" visitorPath="系统首页/主数据/产品类别列表" onClick={this.nav.bind(this)}><i className="fa fa-circle-o"></i>产品类别列表</a></li>
+            </ul>
+          </li>
+      
+      <li className="treeview">
+            <a href="#">
+              <i className="fa fa-table"></i> <span>交易管理</span>
+              <span className="pull-right-container">
+                <i className="fa fa-angle-left pull-right"></i>
+              </span>
+            </a>
+            <ul className="treeview-menu">
+              <li><a component="OrderListComponent" visitorPath="系统首页/交易管理/订单列表" onClick={this.nav.bind(this)}><i className="fa fa-circle-o"></i> 订单管理</a></li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+    </aside>
+    <div className="content-wrapper" style={{ padding: '0 24px 24px' }}>
+      <section className="content">
+        <Breadcrumb style={{ margin: '12px 0' }}>
               {path.map(function (item) {
                 return <Breadcrumb.Item>{item}</Breadcrumb.Item>;
               })
               }
             </Breadcrumb>
-            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 580 }}>
-              {currentComponent}
-            </Content>
-          </Layout>
-        </Layout>
-      </Layout>
-
-    </div>;
+        
+        {currentComponent}
+        
+    </section>
+    </div>
+  
+    <footer className="main-footer">
+      <div className="pull-right hidden-xs">
+        <b>Version</b> 1.0.0
+      </div>
+      <strong>Copyright © 2017</strong> All rights
+      reserved.
+    </footer>
+  </div>;
   }
 }
 
